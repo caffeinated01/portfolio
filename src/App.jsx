5385dfbae6b36ccca0ac26f35a5fbb269adce56e
Javier <javier.has.no.life@gmail.com>
2024-02-12 16:34:23 +0800

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,13 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={
-          <>
-            <h1>Home</h1>
-          </>
+          <div className="mx-5 flex flex-col justify-center items-center h-[90vh] text-center">
+            <h1 className="font-bodoniModa text-5xl"><span className="italic">J</span>AVIER</h1>
+            <h1 className="font-bodoniModa text-5xl">ASPIRING FRONTEND</h1>
+            <h1 className="font-pixelify text-5xl">DEVELOPER <span className="font-bodoniModa">&</span></h1>
+            <h1 className="font-cookie text-5xl">STUDENT <span className="pl-3 font-bodoniModa">IN</span></h1>
+            <h1 className="font-bodoniModa text-5xl">SINGAPORE</h1>
+          </div>
         } />
         <Route path="/about" element={<About />} />
       </Routes>
