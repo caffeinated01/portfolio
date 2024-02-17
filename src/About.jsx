04aec506d35f24fe6976c0d591934fbdd7a5f295
Javier <javier.has.no.life@gmail.com>
2024-02-17 18:53:49 +0800

    update about me

--- src/About.jsx
+++ src/About.jsx
@@ -3,19 +3,19 @@ import AboutP from "./Components/AboutP";
 function About() {
   const aboutParas = [
     {
-      title: "I am Javier",
-      body: "an aspiring front-end developer and a student at Xinmin Secondary School. I enjoy creating small sites and software.",
+      title: "A short intro",
+      body: "Hi, I'm Javier, an aspiring front-end developer and a student at Xinmin Secondary School. I've always been interested in how to create software and websites and decided to learn to when I was 14. Since then, I treated it as a hobby, creating small yet fun projects over the years.",
     },
     {
-      title: "I am hardworking",
-      body: "dedicated to achieving my life goals with hard work and determination, approaching each day with a drive to push myself to the limit. My commitment towards achieving my life goals fuels my passion and propels me towards greatness.",
+      title: "Academics",
+      body: "I am dedicated to achieving my goals with hard work and determination, approaching each day with a drive to push myself to the limit. My commitment towards achieving my goals fuels my passion and propels me towards greatness, and I don't let mere setbacks stop me from doing so.",
     },
     {
-      title: "I am compassionate",
-      body: "I am able to connect with others on a personal level. I work hard to achieve my goals and I do not let mere setbacks stop me from doing so. Beyond the confines of textbooks, I excel in actively listening to people's problems and offering support.",
+      title: "Personality",
+      body: "I bring a vibrant energy and a contagious optimism to every interaction. With a genuine and open heart, I can connecting with others, spreading positivity. Whether it's sparking conversations or rallying my team towards a common goal, I approach each opportunity with enthusiasm and a can-do attitude. I am also able to connect with others on a personal level. I actively listen to people's problems and offer support. ",
     },
     {
-      title: "I am a leader",
+      title: "Leadership",
       body: "As the HOD of Xinmin's Media Club Video Department, I have been instrumental in driving creativity and excellence, leading teams in producing videos for national competitions. By embracing innovation and creativity, I've contributed to the department's ability to create quality productions.",
     },
   ];
--- src/Components/Transition.jsx
+++ src/Components/Transition.jsx
@@ -6,7 +6,7 @@ function Transition(props) {
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -50 }}
-      transition={{ duration: 0.65, ease: [0.22, 1, 0.36, 1] }}
+      transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
     >
       {props.component}
     </motion.div>
