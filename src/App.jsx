6fd738a1d02d7250213767e9cb03c2083926162b
Javier <javier.has.no.life@gmail.com>
2024-02-17 09:45:35 +0800

    fix page transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Home";
 import About from "./About";
@@ -7,6 +6,7 @@ import Achievements from "./Achievements";
 import Lenis from "@studio-freight/lenis";
 import { AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -22,13 +22,14 @@ function App() {
     requestAnimationFrame(raf);
   });
 
+  const location = useLocation();
   return (
     <>
       <div className={darkMode ? "dark" : "light"}>
         <div className="dark:bg-background-dark bg-background-light max-w-screen min-h-screen select-none font-inter dark:text-primary-dark text-primary-light">
           <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-          <AnimatePresence>
-            <Routes>
+          <AnimatePresence mode="wait" initial={false}>
+            <Routes location={location} key={location.pathname}>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/projects" element={<Projects />} />
