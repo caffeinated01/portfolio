35f43a92401efeceb83b97faf9a1a8dbfc219bb0
Javier <javier.has.no.life@gmail.com>
2024-02-19 18:03:11 +0800

    change fonts

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -3,7 +3,7 @@ import Reveal from "./Reveal";
 function AboutSection(props) {
   return (
     <h1 className="md:text-2xl sm:text-xl  flex flex-col max-w-[1100px] px-5">
-      <span className="text-base dark:text-secondary-dark text-secondary-light font-bold">
+      <span className="font-inter text-base dark:text-secondary-dark text-secondary-light font-bold">
         {props.title.toUpperCase()};{" "}
       </span>
       <Reveal component={props.body} />
--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <div className="mx-5 flex flex-col justify-center items-center h-[100vh] text-center">
-        <h1 className="font-bodoniModa text-5xl font-bold">JAVIER</h1>
+        <h1 className="font-inter text-5xl font-semibold">JAVIER</h1>
         <h1 className="font-bodoniModa text-5xl font-semibold">
           ASPIRING FRONT-END
         </h1>
@@ -15,7 +15,7 @@ function Home() {
         <h1 className="font-cookie text-5xl">
           STUDENT <span className="pl-3 font-bodoniModa font-semibold">IN</span>
         </h1>
-        <h1 className="font-bodoniModa text-5xl font-semibold">SINGAPORE</h1>
+        <h1 className="font-mplus text-5xl font-semibold">SINGAPORE</h1>
         <div className="py-6">
           <BsMouse size={30} className="animate-bounce" />
         </div>
